15639743f6d03ff6def927aa117a1246baa17302
SenuraJayadeva <senurajayadeva@gmail.com>
2021-03-26 11:03:49 +0530

    Initial commit

---
+++ src/main/java/com/sltutor/official/SltutorApplication.java
@@ -0,0 +1,13 @@
+package com.sltutor.official;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SltutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SltutorApplication.class, args);
+	}
+
+}
